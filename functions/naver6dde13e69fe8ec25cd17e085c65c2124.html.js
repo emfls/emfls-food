7b19db04040f8490003dbd73153fe7d8f235b4cd
White Smile <whitesmile@Whiteui-MacBookAir.local>
2026-09-15 15:47:47 +0900

    fix: support Naver verification HEAD request

--- functions/naver6dde13e69fe8ec25cd17e085c65c2124.html.js
+++ functions/naver6dde13e69fe8ec25cd17e085c65c2124.html.js
@@ -1,5 +1,10 @@
-export function onRequestGet() {
-  return new Response('naver-site-verification: naver6dde13e69fe8ec25cd17e085c65c2124.html', {
+const body = 'naver-site-verification: naver6dde13e69fe8ec25cd17e085c65c2124.html';
+
+export function onRequest({ request }) {
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    return new Response(null, { status: 405, headers: { allow: 'GET, HEAD' } });
+  }
+  return new Response(request.method === 'HEAD' ? null : body, {
     headers: { 'content-type': 'text/html; charset=UTF-8' },
   });
 }
